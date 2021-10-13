7e9ede0492501353ec6bc65e457bcc9476a61561
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-10-13 12:24:54 -0500

    UI

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 
 window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.title("Flash Card")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=525)
-padlock_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=padlock_img)
-canvas.grid(column=0, row=0, columnspan=2)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="French", fill="black", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="Trouve", fill="black", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0,  columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.grid(row=1, column=1)
+
+
 
 window.mainloop()
