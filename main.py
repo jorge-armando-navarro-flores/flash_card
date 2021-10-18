8396e3050fbc65c0f5046220ae84d9be5782a677
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-10-18 13:15:08 -0500

    is_known

--- main.py
+++ main.py
@@ -3,17 +3,40 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
-data = pandas.read_csv("data/french_words.csv")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+
 
 # ---------------------------- RANDOM WORD ------------------------------- #
 
 
 def next_card():
+    global current_card, timer
+    window.after_cancel(timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_current_img, image=card_front_img)
+    window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_current_img, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    df = pandas.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,10 +44,11 @@ def next_card():
 window = Tk()
 window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
+timer = window.after(3000, flip_card)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_back_img = PhotoImage(file="images/card_back.png")
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_current_img = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", fill="black", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0,  columnspan=2)
@@ -34,7 +58,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
